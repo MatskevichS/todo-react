fe6d57a639c614440505bb413a90004f48796666
MatskevichS <sergeinanit@gmail.com>
2021-02-27 22:01:45 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('test');
+import React from 'react';
+import REactDOM from 'react-dom';
+
+import App from "./components/app";
+
+REactDOM.render(<App />,
+    document.getElementById('root'));
